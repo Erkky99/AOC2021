6ac85dffd4f6a2b27265d15390240c1f50369fb5
Erik Brink <Erkky99@hotmail.com>
2021-12-01 16:10:02 +0100

    Do Day 1 part 1

--- app/Day1.hs
+++ app/Day1.hs
@@ -1,7 +1,23 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
 import SantasSack
+
+f :: [Int] -> [Bool]
+f [] = []
+f [x]       = []
+f (x:y:xys) = (x < y) : f (y:xys)
+
+f' :: (Num a1, Ord a2) => [a2] -> a1
+f' xs = sum $ zipWith (\a b -> if a < b then 1 else 0) xs (tail xs)
 
 main :: IO ()
 main = do
   inp <- getInput 1
-  putStrLn "day 1 is not yet implemented"  
+  --inp <- readFile "input/day1_sub.input"
+  let l = lines inp
+  let r = map (read @Int) l
+  let t = f r
+  let t' = filter id t
+  --print t'
+  print $ length t'
