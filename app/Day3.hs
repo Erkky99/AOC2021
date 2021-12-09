06d5adfa27db1cd14304f986226e0aa0adb7ade7
Erik Brink <Erkky99@hotmail.com>
2021-12-09 12:55:27 +0100

    🤮

--- app/Day3.hs
+++ app/Day3.hs
@@ -1,7 +1,37 @@
 module Main where
 import SantasSack
+import Data.Char (digitToInt)
+
+
+f :: [String] -> Int -> Int
+f (x:xs) n | digitToInt (x !! n) == 1 = 1 + f xs n
+           | otherwise = 0 + f xs n
+
+f [] _ = 0
+
+
+p l x = (length l) `div` 2 > x
+
+p' x = if x > 500 then '1' else '0'
 
 main :: IO ()
 main = do
   inp <- getInput 3
-  putStrLn "day 3 is not yet implemented"  
+  let l = lines inp 
+  let breakOffPoint = (length l) `div` 2
+  -- let g = (\x -> p l (f x))
+  -- let g = f l [0..12]
+
+  let t = [f l x | x <- [0..11]]
+
+  let gamma = [p' x | x <- t]
+
+  let epsilon = map (\x -> if x == '1' then '0' else '1') gamma
+
+  putStrLn $ show gamma
+  putStrLn $ show epsilon
+
+  -- print $ f l 1
+
+  -- print breakOffPoint
+  -- epsilon == complement gamma
--- app/Day4.hs
+++ app/Day4.hs
@@ -1,7 +1,28 @@
+{-# LANGUAGE TypeApplications #-}
 module Main where
 import SantasSack
+
+import Data.List.Split
+
+f :: [String] -> [[Int]]
+f [] = []
+f (r:rs) -- | r == "" = []
+         -- | otherwise 
+         = f' (words r) : f rs
+  where f' = map (read @Int)
 
 main :: IO ()
 main = do
   inp <- getInput 4
-  putStrLn "day 4 is not yet implemented"  
+  let (numbers : boards) = lines inp
+
+  let n = map (read @Int) $ splitOn "," numbers
+
+  print n
+  -- print $ boards !! 1
+
+  let bingoBoards = chunksOf 5 $ f $ filter (not . null) $ tail boards
+
+  print $ bingoBoards !! 0
+
+  putStrLn ""  
